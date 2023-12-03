f87b52606cc5183304dc837059ec12be9c6cb4e0
Navdeep Dadhania <navdeeppatel.1852@gmail.com>
2023-12-03 12:59:55 +0530

    change the card type vertical to horizontal

--- Frontend/src/components/StoryScreens/CardStory.js
+++ Frontend/src/components/StoryScreens/CardStory.js
@@ -13,7 +13,7 @@ const Story = ({ story }) => {
     }
 
     const truncateContent = (content) => {
-        const trimmedString = content.substr(0, 73);
+        const trimmedString = content.substr(0, 350);
         return trimmedString
     }
     const truncateTitle= (title) => {
@@ -22,30 +22,31 @@ const Story = ({ story }) => {
     }
     
     return (
-
+      <Link to={`/story/${story.slug}`} className="story-link">
         <div className="story-card">
-            <Link to={`/story/${story.slug}`} className="story-link">
+          <img
+            className=" story-image"
+            src={`/storyImages/${story.image}`}
+            alt={story.title}
+          />
+          <div className="story-content-wrapper">
+            <h5 className="story-title">
+              {story.title.length > 76
+                ? truncateTitle(story.title) + "..."
+                : story.title}
+            </h5>
 
-                <img className=" story-image" src={`/storyImages/${story.image}`} alt={story.title} />
-                <div className="story-content-wrapper">
-
-                    <h5 className="story-title">
-                        
-                    {story.title.length > 76 ? truncateTitle(story.title)+"..." : story.title
-                    
-                    }
-                    </h5>
-
-
-                    <p className="story-text"dangerouslySetInnerHTML={{__html : truncateContent( story.content) +"..."}}>
-                        </p>
-                    <p className="story-createdAt">{editDate(story.createdAt)} 
-                    </p>
-                </div>
-            </Link>
+            <p
+              className="story-text"
+              dangerouslySetInnerHTML={{
+                __html: truncateContent(story.content) + "...",
+              }}
+            ></p>
+            <p className="story-createdAt">{editDate(story.createdAt)}</p>
+          </div>
         </div>
-
-    )
+      </Link>
+    );
 }
 
 export default Story;
